d7bc462682467a56952b2eb77e6741be24f37282
Sarang Shahane <sarangs@bsf.io>
2019-12-12 16:58:50 +0530

    initial files & folder structure created

---
+++ woo-myaccount.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: WooCommerce My Account
+ * Plugin URI: 
+ * Description: Customize My Account area on your website.
+ * Version: 0.0.1
+ * Author: Sarang Shahane
+ * Author URI: http://sarangshahane.in/
+ * Text Domain: woo-myaccount
+ * WC requires at least: 3.0
+ * WC tested up to: 3.7.0
+ *
+ * @package CartFlows
+ */
+
+/**
+ * Set constants.
+ */
+define( 'MY_ACCOUNT_FILE', __FILE__ );
+
+/**
+ * Loader
+ */
+require_once 'classes/class-woo-myaccount-loader.php';
